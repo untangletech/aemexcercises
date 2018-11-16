a10ab329767880c5b20c9cf77b133b8e9a143a03
rahul-kush <rahul_kush1986@yahoo.co.in>
2018-11-16 16:44:04 +0530

    adding project with servlet logging exercise

---
+++ OSGiServlets/htlmodel-app-project/htlmodel-app-project/core/src/main/java/com/htlmodel/cq/core/models/HelloWorldModel.java
@@ -0,0 +1,24 @@
+package com.htlmodel.cq.core.models;
+
+import javax.annotation.PostConstruct;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+
+@Model (adaptables={Resource.class},resourceType=HelloWorldModel.RESOURCE_TYPE)
+
+public class HelloWorldModel {
+	public static final String RESOURCE_TYPE="llp/components/content/helloworld";
+    @ValueMapValue
+    private String message;
+
+    @PostConstruct
+    protected void init() {
+        message = "\t ADDED AT BACKEND \n" + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
